206bfac17524839a027a65833f8da63ab751a8cd
lyn-codeLab <annluczynski@gmail.com>
2026-03-15 11:06:55 +0000

    added React

---
+++ react/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <><div className="App">
+      <h1>Hello World!</h1>
+    </div><div id="maincontent">
+        <h1>welcome</h1>
+      </div></>
+  );
+}
+
+
+
+
+
+export default App;
